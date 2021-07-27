fec97f38685cf69f1cebd22ec94580a6e898c8e2
MichiyamaKaren <mustarfar@163.com>
2021-07-27 13:37:25 +0800

    update sender

--- twitter2bilibili/sender.py
+++ twitter2bilibili/sender.py
@@ -49,7 +49,7 @@ class BiliSender:
         oid = info['desc']['rid']
         if info['desc']['type'] == 1:   # 1是转发动态，其oid为动态id
             oid = dynamic_id
-        await send_comment(text=text, oid=oid, type_=type_, credential=self.credential)
+        return await send_comment(text=text, oid=oid, type_=type_, credential=self.credential)
 
     @_handle_illegal_word
     async def repost_dynamic(self, text: str, dynamic_id: int):
